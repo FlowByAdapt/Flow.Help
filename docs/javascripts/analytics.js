9d7ed030d12b3354a458d5d12f56e7fc1328383a
ambler289 <109005318+ambler289@users.noreply.github.com>
2026-08-25 08:44:39 +1200

    Google analytics added

---
+++ docs/javascripts/analytics.js
@@ -0,0 +1,17 @@
+// Flow Help — Google Analytics 4
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+gtag("config", "G-74NR0BKB4M");
+
+const script = document.createElement("script");
+script.async = true;
+script.src =
+    "https://www.googletagmanager.com/gtag/js?id=G-74NR0BKB4M";
+
+document.head.appendChild(script);
